349a2bc2a0705d98fe0cb216b288dc47a13081b2
Sebastian Heiden <seb.heiden@gmail.com>
2021-01-31 18:42:11 +0100

    Only allow one checkout_job at a time

--- bot.js
+++ bot.js
@@ -4,44 +4,65 @@ const express = require('express');
 const app = express();
 const autoBuy = require('./auto_buy_nbb.js');
 var id_mappings = {}
+var running_jobs = {};
 
 fs.readdir('configs/', function (err, files) {
     if (err) {
-      console.error("Could not list the directory.", err);
-      process.exit(1);
+        console.error("Could not list the directory.", err);
+        process.exit(1);
     }
-  
+
     files.forEach(function (file) {
-      // Make one pass and make the file complete
-      const raw = fs.readFileSync("configs/" + file, 'utf8');
-      const data = JSON.parse(raw);
-      const id = data.general.id;
-      id_mappings[id] = file;
+        // Make one pass and make the file complete
+        const raw = fs.readFileSync("configs/" + file, 'utf8');
+        const data = JSON.parse(raw);
+        const id = data.general.id;
+        id_mappings[id] = file;
+        running_jobs[id] = false;
     });
-    
+
     app.get('/run/:job_id', async (req, res) => {
         const job_id = req.params.job_id;
+        var success = false;
+        var message;
+
         if (id_mappings[job_id]) {
-            const config_file = id_mappings[job_id];
-            const message = "Starting Job " + job_id;
-            console.log(message);
+            if (running_jobs[job_id]) {
+                message = "job already running"
+            } else {
+                //Mark Job as running
+                running_jobs[job_id] = true;
 
-            const config = require('./configs/' + config_file);
-            await autoBuy(config);
+                const config_file = id_mappings[job_id];
+                const log_message = "Starting Job " + job_id;
+                console.log(log_message);
 
-            res.send(JSON.stringify({
-                message: message,
-                success: true
-            }))
+                const config = require('./configs/' + config_file);
+                try {
+                    await autoBuy(config);
+                    success = true;
+                    message = "Checkout successful"
+                } catch {
+                    success = false;
+                    message = "Checkout failed"
+                }
+
+                //Mark job as completed
+                running_jobs[job_id] = false;
+            }
         } else {
-            res.send(JSON.stringify({
-                success: false
-            }))
+            message = "job_id not found"
         }
+
+        console.log(message);
+        res.send(JSON.stringify({
+            message: message,
+            success: success
+        }))
     });
 
     var port = 3000;
     app.listen(port, () => {
         console.log(`NBB.com AutoBuy Bot listening at http://localhost:${port}`)
     })
-  });
+});
